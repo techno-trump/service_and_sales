d8ff515b7f9005bb8ec4445277d2f7030602a870
Anton Kalinichenko <anton.kalinichenko.dndz@gmail.com>
2018-09-26 09:10:23 +0300

    Add express hello world

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+//import express from 'express';
+//import http from 'http';
+const express = require('express');
+
+console.log('starting');
+
+const app = express();
+
+app.get('/', (req, res) => { res.send('ok') });
+
+app.listen(8080, () => console.log('listening'));
+//http.createServer(app).listen(8080, 'localhost', () => console.log('listening'));
